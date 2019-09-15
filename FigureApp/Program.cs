ca2ca51f897249823dd302519f7251377d79d323
Lebeshkoff <Lebeshkoff@users.noreply.github.com>
2019-09-15 19:05:16 +0300

    Добавьте файлы проекта.

---
+++ FigureApp/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Figures;
+
+namespace FigureApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Figure[] Figures = new Figure[5]; //new объект
+
+            Figures[0] = new Circle(3);
+            Figures[1] = new Rectangle(1, 2);
+            Figures[2] = new Rectangle(2, 3);
+            Figures[3] = new Circle(5);
+            Figures[4] = new Circle(1);
+
+            double P = 0;
+            double S = 0;
+
+            foreach (Figure figure in Figures) // foreach извлекает элемент из коллекции и помещает его в объект
+            {
+                P += figure.CalcP(); //полиморфизм/ позднее связывание
+                S += figure.CalcS(); //вызовет метод в котором нахолится фигура\
+                Console.WriteLine(figure); //будет вызван метод
+            }
+
+            double AvgP = P / Figures.Length;
+            double AvgS = S / Figures.Length;
+
+            Console.WriteLine("+==============================================================+");
+            Console.WriteLine("AvgP = " + Convert.ToString(AvgP));
+            Console.WriteLine("AvgS = " + Convert.ToString(AvgS));
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ FigureApp/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("FigureApp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FigureApp")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("ae696f52-1311-4473-a8e9-522b4e8837ea")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
